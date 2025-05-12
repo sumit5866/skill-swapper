3114005a1d0f82267f6ee0e4a08cef45c796e9e5
sumit5866 <sumitiit586@gmail.com>
2025-05-12 18:58:21 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAFiFGSMf-GjPwlOQdw_NUfyF7rQtok6s8",
+  authDomain: "skill-x-b38ec.firebaseapp.com",
+  projectId: "skill-x-b38ec",
+  storageBucket: "skill-x-b38ec.firebasestorage.app",
+  messagingSenderId: "1067580010790",
+  appId: "1:1067580010790:web:bb1410cbb7a8299e7e51e7",
+  measurementId: "G-EHNRFNSCFF"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
